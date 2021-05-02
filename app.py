ff73736c0451a8ee6aba3156fbb91988c19bb889
tmsteph <tmsteph1290@gmail.com>
2021-05-02 11:27:12 -0700

    commiting

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-import sys
-from flask import Flask, render_template, jsonify, redirect
-import pymongo 
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
 import scrape_mars
 
-sys.setrecursionlimit(2000)
+# Use flask_pymongo to set up mongo connection
 app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
 
-client = pymongo.MongoClient()
-db = client.mars_db
-collection = db.mars_facts
-
-@app.route(/scrape)
-def scrape():
-    mars = scrape_mars.scrape()
-    print("\n\n\n")
-
-    db.mars_facts.insert_one(mars)
-    return "Some scrapped data"
+# route
 @app.route("/")
-def home():
-    mars = list(db.mars_facts.find())
-    print(mars)
+def index():
+    mars = mongo.db.mars.find_one()
+    # print(mars)
     return render_template("index.html", mars = mars)
+
+@app.route("/scrape")
+def scrape():
+    
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    
+    return redirect("http://localhost:5000/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
